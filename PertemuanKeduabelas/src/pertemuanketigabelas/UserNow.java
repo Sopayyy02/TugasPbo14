fab4615ff4a4327385c8e48eccbd3558c805540a
Shofie Fadliya Rahma <rahmashofiesk@gmail.com>
2025-11-21 15:57:18 +0700

    Add files via upload

---
+++ PertemuanKeduabelas/src/pertemuanketigabelas/UserNow.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuanketigabelas;
+
+/**
+ *
+ * @author VIVOBOOK 14
+ */
+public class UserNow {
+      private static String namaUser;
+
+    public static void setNamaUser(String nama) {
+        namaUser = nama;
+    }
+
+    public static String getNamaUser() {
+        return namaUser;
+    }
+}
